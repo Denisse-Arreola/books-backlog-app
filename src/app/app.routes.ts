922ae2a6b48b5ac2fc231765379baa67ed1ee3ae
Denisse <arxie.denisse@gmail.com>
2026-09-26 17:31:26 +0000

    angular material installed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ExploreComponent } from './pages/explore/explore.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'explore',
+        component: ExploreComponent
+    },
+    {
+        path: 'settings',
+        component: SettingsComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
---
+++ src/app/pages/explore/explore.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-explore',
+  styleUrl: './explore.component.scss',
+  templateUrl: './explore.component.html',
+})
+export class ExploreComponent {}
